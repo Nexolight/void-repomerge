b75e3e9fa1e3571b1e7d3e6e3807fc93bd882a0c
nexolight <snow.dream.ch@gmail.com>
2018-05-19 22:33:34 +0200

    Add support for muliple archs

--- autobuild.sh
+++ autobuild.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+# Limitations:
+#	You may only use this with your own packages.
+#	They must be modified by you on the custom repository
+#
+#
+# Variables to pass:
+#	UPSTREAM_REPO - The upstream git repository
+#	CUSTOM_REPO - Yout own custom repository
+#	PRIVATEKEY - Your private key location to sign the packages
+#	SIGNER - a.e. 'someone <someone@mail.com>'
+#	REPO_FOLDER - The folder where the git repository is stored
+#	MAX_JOBS - How many threads are used to build
+#
 # pkgbuild.list
-# <branch> <(rel prebuilt hook | none)> <pkgname>
+# <branch> <(rel prebuilt hook | none)> <pkgname> <arch>
 #
 # Hooks:
 # 	exported values:
@@ -12,18 +25,40 @@
 #		U_VERSION - package version
 #		U_REVISION - package revision
 #		ARCH - The host architecture
+#		B_ARCH - The architecture which is used to build the package
 
 SFILE=`readlink -f $0`
 export SDIR=`dirname "$SFILE"`
 
-UPSTREAM_REPO="https://github.com/voidlinux/void-packages.git"
-CUSTOM_REPO="https://github.com/Nexolight/void-tainted-pkgs.git"
+if [ -z "$UPSTREAM_REPO" ];then
+	UPSTREAM_REPO="https://github.com/voidlinux/void-packages.git"
+fi
 
-PRIVATEKEY="$HOME/.ssh/repo/repokey.pem"
-SIGNER="'nexolight <snow.dream.ch@gmail.com>'"
+if [ -z "$CUSTOM_REPO" ];then
+	CUSTOM_REPO="https://github.com/Nexolight/void-tainted-pkgs.git"
+fi
 
-export REPO_FOLDER="$HOME/git/void-autobuild/repo"
+if [ -z "$PRIVATEKEY" ];then
+	PRIVATEKEY="$HOME/.ssh/repo/repokey.pem"
+fi
+
+if [ -z "$SIGNER" ];then
+	SIGNER="'nexolight <snow.dream.ch@gmail.com>'"
+fi
+
+if [ -z "$REPO_FOLDER" ];then
+	export REPO_FOLDER="$HOME/git/void-autobuild/repo"
+fi
+
+if [ -z "$MAX_JOBS" ];then
+	MAX_JOBS=$(nproc)
+fi
+
 export ARCH=$(uname -m)
+
+function info(){
+echo ">>>> INFO | $1"
+}
 
 function stage(){
 echo ">>>> STAGE | $1"
@@ -52,7 +87,27 @@ fi
 stage "Updating repository"
 git fetch --all
 git checkout origin/master
-./xbps-src binary-bootstrap
+
+ARCHS=""
+NL=$'\n'
+while read -r pkg; do
+	B_ARCH=$(echo "$pkg" | cut -f 4 -d ' ')
+	if [ ! -z "$B_ARCH" ]; then
+		ARCHS="${ARCHS}${B_ARCH}${NL}"
+	fi
+done < "$SDIR/pkgbuild.list"
+
+
+ARCHS=$(echo "$ARCHS" | sort -u)
+
+for HDIR in $ARCHS; do
+	if [ "$ARCH" == "x86_64" ] && [ "$HDIR" == "i686" ] ; then
+		./xbps-src -m "masterdir-x86_64" binary-bootstrap
+		./xbps-src -m "masterdir-i686" binary-bootstrap i686
+	else
+		./xbps-src -m "masterdir-$HDIR" binary-bootstrap
+	fi
+done
 
 while read -r pkg; do
 	if [ -z "$pkg" ]; then
@@ -62,6 +117,7 @@ while read -r pkg; do
 	export BRANCH=$(echo "$pkg" | cut -f 1 -d ' ')
 	PREBUILD_HOOK=$(echo "$pkg" | cut -f 2 -d ' ')
 	export PACKAGE=$(echo "$pkg" | cut -f 3 -d ' ')
+	export B_ARCH="$(echo "$pkg" | cut -f 4 -d ' ')"
 	
 	stage "Building $BRANCH > $PACKAGE"
 	git checkout -f "custom/$BRANCH"
@@ -69,15 +125,50 @@ while read -r pkg; do
 	export U_VERSION=$( grep -oE 'version=[0-9.]+' "./srcpkgs/$PACKAGE/template" | cut -f 2 -d '=')
 	export U_REVISION=$( grep -oE 'revision=[0-9]+' "./srcpkgs/$PACKAGE/template" | cut -f 2 -d '=')	
 
-	stage "Checking file: ./hostdir/binpkgs/$BRANCH/$PACKAGE-${U_VERSION}_${U_REVISION}.$ARCH.xbps"
-	if [ ! -f "./hostdir/binpkgs/$BRANCH/$PACKAGE-${U_VERSION}_${U_REVISION}.$ARCH.xbps" ]; then
+	BASEPKG_PATH="./hostdir/binpkgs/$BRANCH"
+	PKG_LIB=""
+	PKG_ARCH=""
+	PKG_SUBDIR=""
+	PKG_EXTRA_SUBDIR=""
+
+	case "$B_ARCH" in
+		*)
+			PKG_ARCH="$B_ARCH"
+			PKG_EXTRA_SUBDIR="nonfree"
+		;;
+	esac
+
+	PKG_FILES="
+		$BASEPKG_PATH/$PKG_SUBDIR/${PACKAGE}${PKG_LIB}-${U_VERSION}_${U_REVISION}.${PKG_ARCH}.xbps
+		$BASEPKG_PATH/$PKG_SUBDIR/${PACKAGE}${PKG_LIB}-${U_VERSION}_${U_REVISION}.noarch.xbps
+		$BASEPKG_PATH/$PKG_SUBDIR/${PKG_EXTRA_SUBDIR}/${PACKAGE}${PKG_LIB}-${U_VERSION}_${U_REVISION}.${PKG_ARCH}.xbps
+                $BASEPKG_PATH/$PKG_SUBDIR/${PKG_EXTRA_SUBDIR}/${PACKAGE}${PKG_LIB}-${U_VERSION}_${U_REVISION}.noarch.xbps
+		"
+	
+	NEEDS_BUILD=1
+	for PKG_FILE in $PKG_FILES; do
+		info "Checking for file: $PKG_FILE"
+		if [ -f "$PKG_FILE" ]; then
+			info "Found: $PKG_FILE"
+			NEEDS_BUILD=0
+			break
+		fi
+	done
+
+	IS_CC=1
+	if [ "$B_ARCH" == "i686" ] && [ "$ARCH" == "x86_64" ]; then
+		IS_CC=0
+	fi
+	if [ "$ARCH" == "$B_ARCH" ] || [ "$ARCH-musl" == "$B_ARCH" ]; then
+		IS_CC=0
+	fi 
+	
+	if [ "$NEEDS_BUILD" == 1 ]; then
 		stage "$PACKAGE got an update > Rebuilding it"
 
 		git branch -D "$BRANCH"
-		git checkout -b "$BRANCH" -f "custom/$BRANCH"
-		git pull
-		git merge -s ours "origin/master"
-		
+		git checkout -b "$BRANCH" -f custom/"$BRANCH"
+		git merge -X ours --no-commit --no-ff origin/master		
 
 		if [ "$PREBUILD_HOOK" != "none" ]; then
 			stage "Executing prebuild hook: $PREBUILD_HOOK"
@@ -87,12 +178,24 @@ while read -r pkg; do
 				warn "The hook specified for the package doesn't exist"
 			fi	
 		fi
+		
 
-		./xbps-src clean "$PACKAGE" &> /dev/null
-		./xbps-src pkg -j $(nproc) "$PACKAGE"
-		xbps-rindex -a -f ./hostdir/binpkgs/$BRANCH/*.xbps
-		xbps-rindex --sign --signedby "$SIGNER" --privkey "$PRIVATEKEY" "$REPO_FOLDER/hostdir/binpkgs/$BRANCH/"
-		xbps-rindex --sign-pkg --signedby "$SIGNER" --privkey "$PRIVATEKEY" "$REPO_FOLDER"/hostdir/binpkgs/"$BRANCH"/*.xbps
+		CC_ARCH=""
+		if [ "$IS_CC" == 1 ]; then
+			CC_ARCH="-a $B_ARCH"
+		fi 
+
+		./xbps-src -m "masterdir-$B_ARCH" "$CC_ARCH" clean "$PACKAGE"
+		./xbps-src -m "masterdir-$B_ARCH" -j $MAX_JOBS "$CC_ARCH" pkg "$PACKAGE" 
+		
+		SIGNDIRS="$BRANCH $BRANCH/nonfree $BRANCH/multilib $BRANCH/multilib/nonfree"
+		
+		for SIGNDIR in $SIGNDIRS; do
+			BINDIR="./hostdir/binpkgs/$SIGNDIR"
+			xbps-rindex -a -f "$BINDIR"/*.xbps
+			xbps-rindex --sign --signedby "$SIGNER" --privkey "$PRIVATEKEY" "$BINDIR"
+			xbps-rindex --sign-pkg --signedby "$SIGNER" --privkey "$PRIVATEKEY" "$BINDIR"/*.xbps
+		done
 	fi
 done < "$SDIR/pkgbuild.list"
 
