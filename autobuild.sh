b3d10168284be50775e598fd560b5d5cb495a126
dummy <dummy@dummy.com>
2018-07-04 15:47:24 +0200

    fix update problem

--- autobuild.sh
+++ autobuild.sh
@@ -107,16 +107,19 @@ done < "$SDIR/pkgbuild.list"
 ARCHS=$(echo "$ARCHS" | sort -u)
 
 for HDIR in $ARCHS; do
-	if [ "$ARCH" == "x86_64" ] && [ "$HDIR" == "i686" ] ; then
-		./xbps-src -m "masterdir-x86_64" binary-bootstrap
-		./xbps-src -m "masterdir-x86_64" bootstrap-update
+	for RUN in {1..2};do
+		info "Update stage $RUN for $HDIR"
+		if [ "$ARCH" == "x86_64" ] && [ "$HDIR" == "i686" ] ; then
+			./xbps-src -m "masterdir-x86_64" binary-bootstrap
+			./xbps-src -m "masterdir-x86_64" bootstrap-update
 
-		./xbps-src -m "masterdir-i686" binary-bootstrap i686
-		./xbps-src -m "masterdir-i686" bootstrap-update i686
-	else
-		./xbps-src -m "masterdir-$HDIR" binary-bootstrap
-		./xbps-src -m "masterdir-$HDIR" bootstrap-update
-	fi
+			./xbps-src -m "masterdir-i686" binary-bootstrap i686
+			./xbps-src -m "masterdir-i686" bootstrap-update i686
+		else
+			./xbps-src -m "masterdir-$HDIR" binary-bootstrap
+			./xbps-src -m "masterdir-$HDIR" bootstrap-update
+		fi
+	done
 done
 
 while read -r pkg; do
